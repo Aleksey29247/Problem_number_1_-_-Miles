6e15261cafaaa8ef04b953ba09f6fd47c5f32f2e
Aleksey29247 <alik29247@gmail.com>
2021-12-25 23:40:36 +0700

    TestJava

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String [] args) {
+        int ticketPrice = 2000;
+        int mile = 0;
+        int mcast =20;
+        mile = ticketPrice / mcast;
+        System.out.println("ваши балы:"+mile );
+    }
+
+}
